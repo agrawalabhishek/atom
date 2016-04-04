cb46dce1b4cc015f4f83f99104cb5c349e46730a
Abhishek Agrawal <abhishek.agrawal@protonmail.com>
2016-04-04 14:51:40 +0200

    Set up files for diagnostics

--- ProjectFiles.cmake
+++ ProjectFiles.cmake
@@ -2,6 +2,16 @@
 # Distributed under the MIT License.
 # See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
 
+# Set project main file.
+set(MAIN_SRC
+  "${SRC_PATH}/main.cpp"
+)
+
+# Set project source files.
+#set(SRC
+# "${SRC_PATH}/tools.cpp"
+#)
+
 # Set project test source files.
 set(TEST_SRC
   "${TEST_SRC_PATH}/testAtom.cpp"
---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+// Program to diagnose the cartesian state to two line element converter.
+// Copyright (c) 2016, Abhishek Agrawal (abhishek.agrawal@protonmail.com)
+
+#include <iostream>
+#include <random>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include <libsgp4/DateTime.h>
+#include <libsgp4/Eci.h>
+#include <libsgp4/Globals.h>
+#include <libsgp4/SGP4.h>
+#include <libsgp4/Tle.h>
+#include <libsgp4/Vector.h>
+
+#include <Astro/astro.hpp>
+#include <SML/sml.hpp>
+
+#include "Atom/convertCartesianStateToTwoLineElements.hpp"
+
+int main( void )
+{
+    std::cout << "hello" << std::endl;
+    return 0;
+}
